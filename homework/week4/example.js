44a7d929601e4361a7e9bf8e6805fd3577ef00ed
snaag <04_miffy@naver.com>
2021-03-02 02:56:09 +0900

    4주차 과제

---
+++ homework/week4/example.js
@@ -0,0 +1,13 @@
+// function 키워드를 사용해 함수 만들기
+function subtract1(a, b) {
+    return a - b;
+}
+
+subtract1(5, 3);
+
+// 화살표 함수 만들기
+const subtract2 = (a, b) => {
+    return a - b;
+};
+
+subtract2(5, 3);
---
+++ homework/week4/homework.js
@@ -0,0 +1,16 @@
+// function 키워드를 사용해 함수 만들기
+function sum(a, b) {
+    return a + b;
+}
+
+function sayHi() {
+    console.log("안녕하세요");
+}
+
+function me(name, age) {
+    console.log("안녕하세요,");
+    console.log("제 이름은 ", name, " 입니다.");
+    console.log("저는 ", age, " 살 입니다.");
+}
+
+// TODO: 화살표 함수로 작성해보기

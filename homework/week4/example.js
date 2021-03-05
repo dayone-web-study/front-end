6404c1e567d8f128d90b98ca256a010bc6393e89
Lee Junhee <junhee0110@gmail.com>
2021-03-05 15:19:31 +0900

    Add week4 homework submission

--- homework/week4/example.js
+++ homework/week4/example.js
@@ -3,11 +3,11 @@ function subtract1(a, b) {
     return a - b;
 }
 
-subtract1(5, 3);
+console.log(subtract1(5, 3));
 
 // 화살표 함수 만들기
 const subtract2 = (a, b) => {
     return a - b;
 };
 
-subtract2(5, 3);
+console.log(subtract2(5, 3));
--- homework/week4/homework.js
+++ homework/week4/homework.js
@@ -14,3 +14,30 @@ function me(name, age) {
 }
 
 // TODO: 화살표 함수로 작성해보기
+
+const sum_2 = (a, b) => a + b;
+
+const sayHi_2 = () => console.log("안녕하세요");
+
+const me_2 = (name, age) =>
+{
+    console.log("안녕하세요,");
+    console.log("제 이름은 ", name, " 입니다.");
+    console.log("저는 ", age, " 살 입니다.");
+};
+
+// Test
+
+console.log("------function keyword------")
+console.log(sum(2,3));
+console.log();
+sayHi();
+console.log();
+me("이준희", 21.5);
+
+console.log("\n------arrow function------")
+console.log(sum_2(2,3));
+console.log();
+sayHi_2();
+console.log();
+me_2("이준희", 21.5);

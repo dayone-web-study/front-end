8f6cbc1a3951ddc31614c5bea029306649c40252
kyuung <mel1521@naver.com>
2021-03-06 01:07:37 +0900

    week-4-homework

--- homework/week4/homework.js
+++ homework/week4/homework.js
@@ -14,3 +14,17 @@ function me(name, age) {
 }
 
 // TODO: 화살표 함수로 작성해보기
+const sum2 = (a,b) => {
+    return a+b;
+};
+sum2(5,3);
+
+const sayHi2 = (v) => console.log(v);
+sayHi2("안녕하세요");
+
+const me2 = (name2, age2) => {
+    console.log("안녕하세요,");
+    console.log("제 이름은 ", name2, " 입니다.");
+    console.log("저는 ", age2, " 살 입니다.");
+}
+me2("kyuung",10);
